67e64fe85865c95fc1517f2c4ab03863e3d1be63
Jiri Bruchanov <jbruchanov@gmail.com>
2017-03-29 17:40:26 +0100

    Add json11 library

--- test/sample/_sample.cpp
+++ test/sample/_sample.cpp
@@ -1,9 +1,10 @@
 //
-// Created by scurab on 29/03/17.
+// Created by jbruchanov on 29/03/17.
 //
 
 #include <googletest/include/gtest/gtest.h>
 #include "../../src/SimpleClass.h"
+#include "../../json11/json11.hpp"
 
 TEST(Sample, SimpleAssertTrue) {
     bool value = true;
@@ -14,4 +15,12 @@ TEST(Sample, ProjectFolderAccess) {
     SimpleClass c;
     int result = c.add(1, 2);
     ASSERT_EQ(3, result);
+}
+
+
+TEST(Sample, Json) {
+    json11::Json result = json11::Json::object {
+            { "a", 1 },
+            { "b", 2 }};
+    ASSERT_EQ("{\"a\": 1, \"b\": 2}", result.dump());
 }
