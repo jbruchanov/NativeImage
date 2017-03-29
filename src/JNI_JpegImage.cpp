e085819c65a5d5f700000a3a33bbe9ee817afed2
Jiri Bruchanov <jbruchanov@gmail.com>
2017-03-29 13:59:03 +0100

    Add reference to JNI

---
+++ src/JNI_JpegImage.cpp
@@ -0,0 +1,35 @@
+//
+// Created by scurab on 29/03/17.
+//
+
+#include "JNI_JpegImage.h"
+/*
+ * Class:     com_scurab_andriod_nativeimage_JPEGImage
+ * Method:    loadImage
+ * Signature: (Ljava/lang/String;)V
+ */
+JNIEXPORT void JNICALL Java_com_scurab_andriod_nativeimage_JPEGImage_loadImage
+(JNIEnv *env, jobject obj, jstring string) {
+
+}
+
+/*
+ * Class:     com_scurab_andriod_nativeimage_JPEGImage
+ * Method:    getDetails
+ * Signature: ()Ljava/lang/String;
+ */
+JNIEXPORT jstring JNICALL Java_com_scurab_andriod_nativeimage_JPEGImage_getDetails
+        (JNIEnv *env, jobject obj) {
+
+}
+
+/*
+ * Class:     com_scurab_andriod_nativeimage_JPEGImage
+ * Method:    dispose
+ * Signature: ()V
+ */
+JNIEXPORT void JNICALL Java_com_scurab_andriod_nativeimage_JPEGImage_dispose
+(JNIEnv *env, jobject obj) {
+
+}
+
