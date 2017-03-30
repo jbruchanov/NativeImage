5da3a938af217b180821255bebdc255e42d552d4
Jiri Bruchanov <jbruchanov@gmail.com>
2017-03-30 21:34:43 +0100

    Add setPixels for jni bridge

--- src/JNI_JpegImage.cpp
+++ src/JNI_JpegImage.cpp
@@ -2,10 +2,18 @@
 // Created by jbruchanov on 29/03/17.
 //
 
+#ifdef DESKTOP
+
+#include "fakers/AndroidBitmap.h"
+#else
+#include "android/bitmap.h"
+#endif
+
 #include "JNI_JpegImage.h"
 #include "../json11/json11.hpp"
 #include "JPEGImage.hpp"
 #include "LogHelper.h"
+#include "Errors.h"
 
 #define CLASS_NAME "com/scurab/andriod/nativeimage/JPEGImage"
 #define METHOD_SET_NATIVE_REF "onSetNativeRef"
@@ -37,14 +45,6 @@ JNIEXPORT void JNICALL Java_com_scurab_andriod_nativeimage_JPEGImage__1init
     JPEGImage *image = new JPEGImage();
     LOGD("JPEGImagePointer:%ld", (long)image);
     env->CallVoidMethod(obj, methodId, (jlong)image);
-}
-
-JPEGImage* getObject(JNIEnv *env, jobject obj) {
-    jclass clazz = env->FindClass(CLASS_NAME);
-    jmethodID methodId = env->GetMethodID(clazz, METHOD_GET_NATIVE_REF, "()J");//long getNativeRef()
-    // Call the method on the object
-    jlong ptr = env->CallLongMethod(obj, methodId);
-    return (JPEGImage*)ptr;
 }
 
 /*
@@ -100,5 +100,47 @@ JNIEXPORT void JNICALL Java_com_scurab_andriod_nativeimage_JPEGImage__1dispose
     delete image;
 }
 
+/*
+ * Class:     com_scurab_andriod_nativeimage_JPEGImage
+ * Method:    _setPixels
+ * Signature: (Landroid/graphics/Bitmap;)V
+ */
+JNIEXPORT jint JNICALL Java_com_scurab_andriod_nativeimage_JPEGImage__1setPixels
+        (JNIEnv *env, jobject obj, jobject bitmap) {
+
+    AndroidBitmapInfo info;
+    int v;
+    v = AndroidBitmap_getInfo(env, bitmap, &info);
+    if (v != ANDROID_BITMAP_RESULT_SUCCESS) {
+        return v;
+    }
+
+    JPEGImage *image = getObject(env, obj);
+    ImageMetaData metaData = image->getMetaData();
+    if (metaData.pixelSize() != (info.height * info.width)) {
+        LOGE("Invalid resolution loadedImage:%dx%d vs bitmap:%dx%d", metaData.imageWidth, metaData.imageHeight, info.width, info.height);
+        return NOT_SAME_RESOLUTION;
+    }
+
+    if (info.format != ANDROID_BITMAP_FORMAT_RGBA_8888) {
+        LOGE("Invalid bitmap format, expected RGBA_8888 was:%d (<android/Bitmap.h>@AndroidBitmapFormat)", info.format);
+        return INVALID_BITMAP_FORMAT;
+    }
+
+    int *ptr;
+    v = AndroidBitmap_lockPixels(env, bitmap, (void **) &ptr);
+    if (v != ANDROID_BITMAP_RESULT_SUCCESS) {
+        return v;
+    }
+
+    image->setPixels(ptr);
+
+    v = AndroidBitmap_unlockPixels(env, bitmap);
+    if(v != ANDROID_BITMAP_RESULT_SUCCESS) {
+        LOGE("!!! Unable to unlock pixels, something BAD will happen soon !!!");
+    }
+    //well not sure what to do here if it's not unlocked ?
+    return (jint) v;
+}
 
 
