8e75bc27cb83eb99cfe82bb8cfae71cbceb835ad
Jiri Bruchanov <jbruchanov@gmail.com>
2017-03-30 21:33:18 +0100

    Update ImageMetaData struct

--- src/ImageMetaData.h
+++ src/ImageMetaData.h
@@ -1,13 +1,17 @@
 //
-// Created by scurab on 29/03/17.
+// Created by jbruchanov on 29/03/17.
 //
 
 #ifndef NATIVEIMAGEAPP_IMAGEMETADATA_H
 #define NATIVEIMAGEAPP_IMAGEMETADATA_H
 
 struct ImageMetaData {
-    unsigned int imageWidth;
-    unsigned int imageHeight;
+    int imageWidth;
+    int imageHeight;
+    int componentSize;
+
+public:
+    int pixelSize() { return imageWidth * imageHeight; }
 };
 
 #endif //NATIVEIMAGEAPP_IMAGEMETADATA_H
