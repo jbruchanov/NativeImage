453bc551af0df483590b3edb4823e97b5492cca3
Jiri Bruchanov <jbruchanov@gmail.com>
2017-04-12 23:09:03 +0100

    Update build scripts

--- android/build.cmd
+++ android/build.cmd
@@ -1,3 +1,10 @@
+@Echo Off
+
+set DEBUG=0
+IF "%1"=="1" (set DEBUG=1)
+
+IF %DEBUG%==1 (echo APP_ABI := x86 armeabi-v7a > Application.mk) ELSE (echo APP_ABI := all > Application.mk)
+
 ::delete old files in case something happend
 rmdir libs /S /Q
 rmdir obj /S /Q
@@ -8,9 +15,17 @@ copy Android.mk jni
 copy Application.mk jni
 
 cd jni
+
 ::be sure it's targeting crystax folder! eg '/home/user/crystax-ndk-10.3.2'
-call %ANDROID_NDK%\ndk-build NDK_DEBUG=1
+rem call %ANDROID_NDK%\ndk-build NDK_DEBUG=1
+echo DEBUG=%DEBUG%
+call %ANDROID_NDK%\ndk-build NDK_DEBUG=%DEBUG%
 
 cd ..
 rmdir jni /S /Q
+del libjpeg.so /S
+del libpng.so /S
+del libturbojpeg.so /S
+
+IF %DEBUG%==0 (rmdir obj /S /Q)
 pause
--- android/build.sh
+++ android/build.sh
@@ -1,4 +1,14 @@
 #~/bin/sh
+DEBUG=0
+if [ "$1" == "1" ]; then
+	DEBUG=1
+fi
+
+if [ $DEBUG == 1 ]; then
+	echo "APP_ABI := x86 armeabi-v7a" > Application.mk
+else
+	echo "APP_ABI := all" > Application.mk
+fi
 
 JNI="jni" #must be 'jni'
 
@@ -15,13 +25,18 @@ cp Application.mk ./$JNI
 
 cd $JNI
 #be sure it's targeting crystax folder! eg '/home/user/crystax-ndk-10.3.2'
-eval "$ANDROID_NDK/ndk-build NDK_DEBUG=1"
+echo DEBUG=$DEBUG
+eval "$ANDROID_NDK/ndk-build NDK_DEBUG=$DEBUG"
 
 cd -
 rm -R $JNI 2> /dev/null 
 
-rm -R /media/sf_Shared/libs
-rm -R /media/sf_Shared/obj
-# mkdir /media/sf_Shared/libs
-cp -a ./libs /media/sf_Shared
-cp -a ./obj /media/sf_Shared
+find . -name "libjpeg.so" -type f -delete
+find . -name "libpng.so" -type f -delete
+find . -name "libturbojpeg.so" -type f -delete
+find . -name "gdbserver" -type f -delete
+find . -name "gdb.setup" -type f -delete
+
+if [ $DEBUG == 0 ]; then
+	rm -R obj 2> /dev/null 
+fi
